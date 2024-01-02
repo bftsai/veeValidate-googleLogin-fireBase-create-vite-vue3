6a44e2ec0d06a2817f5c3ab501fbb88ad2c2ffbd
bf_tsai <tsai19911026@gmail.com>
2024-01-02 20:19:26 +0800

    deploy

--- assets/AboutView-HM5ZWZEM.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as t,a as s}from"./index-JWf8fDT9.js";import"https://kit.fontawesome.com/96a7520586.js";const c={},a={class:"about"},n=s("h1",null,"This is an about page",-1),_=[n];function r(i,d){return o(),t("div",a,_)}const f=e(c,[["render",r]]);export{f as default};
